86bd36ec5547560dd5425fdef289ef56bd55e986
Camila Gallegos <camigm1@gmail.com>
2022-08-04 18:24:25 -0600

    updated OOP

--- app.js
+++ app.js
@@ -2,3 +2,32 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+class Person {
+  constructor(name, pets, residence, hobbies) {
+    this.name = name;
+    this.pets = pets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+  }
+  info() {
+    console.log(
+      `Hello! My name is ${this.name}, I have ${this.pets}  pets and I live in ${this.residence}. My hobbies also include ${this.hobbies}`
+    );
+  }
+}
+
+class Coder extends Person {
+  constructor(name, pets, residence, hobbies, occupation) {
+    super(name, pets, residence, hobbies);
+    this.occupation = "FullStack Developer";
+  }
+  greeting() {
+    console.log(`Hello! I'm a ${this.occupation}`);
+  }
+}
+
+const camila = new Person("camila", 2, "Salt Lake City", ["tennis", "soccer"]);
+const coderCami = new Coder();
+
+console.log(coderCami.greeting());
+console.log(camila.info());
